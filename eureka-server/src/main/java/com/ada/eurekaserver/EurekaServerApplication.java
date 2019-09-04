3db78986c9659aa6bcfd63bf9ce2aabb9f6c6a35
Ada0910 <l355948107@qq.com>
2019-09-04 19:48:40 +0800

    update

--- eureka-server/src/main/java/com/ada/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/ada/eurekaserver/EurekaServerApplication.java
@@ -1,16 +1,18 @@
 package com.ada.eurekaserver;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class EurekaServerApplication  {
 
     public static void main(String[] args) {
-      //  SpringApplication.run(EurekaServerApplication.class, args);
-        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+        SpringApplication.run(EurekaServerApplication.class, args);
+      //  SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
 
     }
 
